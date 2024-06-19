3d535e0eaed2026200f0d9a3b99e9ad1c323596d
Sihab Hossain <sihabhossain380@gmail.com>
2024-06-19 13:50:15 +0600

    error handlers added

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,7 +1,19 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
 
 const createUserIntoDB = async (payload: TUser) => {
+  // Check if the user already exists
+  const existingUser = await User.findOne({ email: payload.email });
+  if (existingUser) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "User already exists with this email"
+    );
+  }
+
+  // Create new user
   const result = await User.create(payload);
   return result;
 };
