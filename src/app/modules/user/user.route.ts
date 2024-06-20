d9acd5b333fde63b62f8812dd40bbc588c795605
Sihab Hossain <sihabhossain380@gmail.com>
2024-06-20 17:11:53 +0600

    auth validation added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,7 @@ import { userControllers } from "./user.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { UserValidations } from "./user.validation";
 import { authControllers } from "../auth/auth.controller";
+import { AuthValidation } from "../auth/auth.validation";
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ router.post(
   userControllers.createUser
 );
 
-router.post("/login", authControllers.login);
+router.post(
+  "/login",
+  validateRequest(AuthValidation.loginValidationSchema),
+  authControllers.login
+);
 
 export const userRoutes = router;
