41f1fccb8a8c40664ed30d1e5cf70c885af4da2e
Sihab Hossain <sihabhossain380@gmail.com>
2024-06-21 23:19:47 +0600

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
-import config from './app/config'
-import mongoose from 'mongoose'
-import { Server } from 'http'
-import { app } from './app'
+import config from "./app/config";
+import mongoose from "mongoose";
+import { Server } from "http";
+import { app } from "./app";
 
-let server: Server
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let server: Server;
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    await mongoose.connect(config.database_url as string);
 
     server = app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-main()
+main();
